5ee79da2acc80f4b743c9eb5448b1bb9c99db6ce
vlad_pavliuk <pavliuk.vlad@gmail.com>
2018-04-07 00:31:07 +0300

    Added main layout

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -2,7 +2,8 @@ function loadDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("demo").innerHTML = this.responseText;
+            console.log(this.response);
+            // document.getElementById("demo").innerHTML = this.responseText;
         }
     };
     xhttp.open("GET", "http://localhost:8000", true);
--- server/controllers/MainController.php
+++ server/controllers/MainController.php
@@ -4,6 +4,8 @@ class MainController extends Controller
 {
     public function indexAction()
     {
+        header('Content-type: application/json');
+        header("Access-Control-Allow-Origin: *");
         echo(json_encode(['kus' => 'ne_kus']));
     }
 }
